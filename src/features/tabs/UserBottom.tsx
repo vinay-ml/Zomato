800016555c93d330d6c66add137927fdf0a10166
vinay-ml <vinay.mern@gmail.com>
2024-12-30 16:47:55 +0530

    corrected

--- src/features/tabs/UserBottom.tsx
+++ src/features/tabs/UserBottom.tsx
@@ -1,22 +1,22 @@
-import DeliveryScreen from '@features/delivery/DeliveryScreen';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React, {FC} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import DeliveryScreen from '@features/delivery/DeliveryScreen';
 
 const Tab = createBottomTabNavigator();
 
-const UserBottomTab: FC = () => {
+const UserBottom = () => {
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
         tabBarHideOnKeyboard: true,
       }}>
-      <Tab.Screen name="Delivery" component={<DeliveryScreen />} />
-      <Tab.Screen name="Reorder" component={<DeliveryScreen />} />
-      <Tab.Screen name="Dining" component={<DeliveryScreen />} />
-      <Tab.Screen name="Live" component={<DeliveryScreen />} />
+      <Tab.Screen name="Delivery" component={DeliveryScreen} />
+      <Tab.Screen name="Reorder" component={DeliveryScreen} />
+      <Tab.Screen name="Dining" component={DeliveryScreen} />
+      <Tab.Screen name="Live" component={DeliveryScreen} />
     </Tab.Navigator>
   );
 };
 
-export default UserBottomTab;
+export default UserBottom;
